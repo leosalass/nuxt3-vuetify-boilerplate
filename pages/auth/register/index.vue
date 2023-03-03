f97149ba510f8c7800f69aa307e9f6c17f3dcedc
Leonardo Salas <leosalass@gmail.com>
2023-03-03 00:32:39 -0500

    form improvements

--- pages/auth/register/index.vue
+++ pages/auth/register/index.vue
@@ -8,40 +8,60 @@
       >
         <v-sheet class="ma-2 pa-2">
           <v-card :loading="false" min-width="500">
+            <v-alert
+              border="start"
+              color="primary"
+              density="comfortable"
+              class="rounded-t-md rounded-b-0 text-center"
+              variant="flat"
+            >
+              User Registration
+            </v-alert>
             <v-form>
               <v-card-text>
                 <v-row cols="12">
                   <v-col xs="12" md="12">
                     <v-text-field
+                      :ripple="false"
                       clearable
+                      clear-icon="fa-solid fa-delete-left"
                       label="Full name"
-                      prepend-icon="fa-solid fa-user"
+                      prepend-inner-icon="fa-solid fa-user"
                       variant="underlined"
                       hint="Enter your full name"
+                      persistent-hint
                     ></v-text-field>
                   </v-col>
                   <v-col xs="12" md="12">
                     <v-text-field
+                      :ripple="false"
                       clearable
+                      clear-icon="fa-solid fa-delete-left"
                       label="Email"
-                      prepend-icon="fa-solid fa-envelope"
+                      prepend-inner-icon="fa-solid fa-envelope"
                       variant="underlined"
                       hint="Enter your email to access this website"
+                      persistent-hint
                     ></v-text-field>
                   </v-col>
                   <v-col xs="12" md="12">
                     <v-text-field
                       clearable
+                      clear-icon="fa-solid fa-delete-left"
                       label="Password"
-                      prepend-icon="fa-solid fa-lock"
+                      prepend-inner-icon="fa-solid fa-lock"
+                      :append-icon="`fa-solid ${passwordIcon}`"
+                      @click:append="showPassword = !showPassword"
                       variant="underlined"
                       hint="Enter your password to access this website"
+                      persistent-hint
+                      :type="showPassword ? 'text' : 'password'"
                     ></v-text-field>
                   </v-col>
                 </v-row>
                 <v-row>
                   <v-col class="d-flex align-center justify-center">
-                    <v-btn color="primary">Register</v-btn>
+                    <v-btn color="primary">Register now</v-btn>
                   </v-col>
                 </v-row>
               </v-card-text>
@@ -57,11 +77,17 @@
 export default {
   data() {
     return {
+      showPassword: false,
       login: {
         visible: false,
         wH: 0,
       },
     };
+  },
+  computed: {
+    passwordIcon(){
+        return this.showPassword ? 'fa-eye-slash' : 'fa-eye';
+    }
   },
   mounted() {
     this.login.wH = window.innerHeight;
@@ -70,7 +96,11 @@ export default {
 };
 </script>
 <style>
-.v-btn:hover .v-btn__content {
-  /*color: red !important;*/
+.text-center {
+  text-align: center;
+}
+.v-input__control.v-input--has-state {
+  padding-top: 14px !important;
+  padding-bottom: 6px !important;
 }
 </style>
