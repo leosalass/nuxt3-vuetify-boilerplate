21f0336998c48911a11fc2269d6f3ee966637a85
Leonardo Salas <leosalass@gmail.com>
2023-03-03 07:56:20 -0500

    signup improvements for mobile

--- pages/auth/register/index.vue
+++ pages/auth/register/index.vue
@@ -2,89 +2,83 @@
   <div>
     <div class="d-flex justify-center mb-6 bg-surface-variant">
       <v-sheet
-        class="d-flex align-center mb-6"
+        class="d-flex align-center mb-6 pa-6"
         :height="login.wH"
         v-if="login.visible"
-        max-width="500"
       >
-        <v-sheet class="ma-2 pa-2">
-          <v-card
-            :loading="false"
-            min-width="450"
-            elevation="2"
-            class="rounded-lg"
-          >
-            <v-row> <v-col class="text-center"></v-col> </v-row>
-            <v-row>
-              <v-col class="text-center"><h1>Signup</h1></v-col>
-            </v-row>
-            <v-form>
-              <v-card-text>
-                <v-row cols="12">
-                  <v-col xs="12" md="12">
-                    <v-text-field
-                      :ripple="false"
-                      clearable
-                      clear-icon="fa-solid fa-delete-left"
-                      label="Full name"
-                      prepend-inner-icon="fa-solid fa-user"
-                      variant="outlined"
-                      hint="Enter your full name"
-                      persistent-hint
-                      density="compact"
-                      class="medium_icon"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col xs="12" md="12">
-                    <v-text-field
-                      :ripple="false"
-                      clearable
-                      clear-icon="fa-solid fa-delete-left"
-                      label="Email"
-                      prepend-inner-icon="fa-solid fa-envelope"
-                      variant="outlined"
-                      hint="Enter your email to access this website"
-                      persistent-hint
-                      density="compact"
-                      class="medium_icon"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col xs="12" md="12">
-                    <v-text-field
-                      clearable
-                      clear-icon="fa-solid fa-delete-left"
-                      label="Password"
-                      prepend-inner-icon="fa-solid fa-lock"
-                      :append-inner-icon="`fa-solid ${passwordIcon}`"
-                      @click:append-inner="showPassword = !showPassword"
-                      variant="outlined"
-                      hint="Enter your password to access this website"
-                      persistent-hint
-                      :type="showPassword ? 'text' : 'password'"
-                      counter
-                      maxlength="32"
-                      density="compact"
-                      class="medium_icon"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col class="d-flex align-center justify-center">
-                    <v-btn color="primary" width="100%">Signup</v-btn>
-                  </v-col>
-                </v-row>
+        <v-card
+          max-width="550"
+          :loading="false"
+          elevation="3"
+          class="rounded-lg"
+        >
+          <v-row> <v-col class="text-center"></v-col> </v-row>
+          <v-row>
+            <v-col class="text-center"><h1>Signup</h1></v-col>
+          </v-row>
+          <v-form>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    :ripple="false"
+                    clearable
+                    clear-icon="fa-solid fa-delete-left"
+                    label="Full name"
+                    prepend-inner-icon="fa-solid fa-user"
+                    variant="outlined"
+                    hint="Enter your full name"
+                    density="compact"
+                    class="medium_icon"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    :ripple="false"
+                    clearable
+                    clear-icon="fa-solid fa-delete-left"
+                    label="Email"
+                    prepend-inner-icon="fa-solid fa-envelope"
+                    variant="outlined"
+                    hint="Enter your email to access this website"
+                    density="compact"
+                    class="medium_icon"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    clearable
+                    clear-icon="fa-solid fa-delete-left"
+                    label="Password"
+                    prepend-inner-icon="fa-solid fa-lock"
+                    :append-inner-icon="`fa-solid ${passwordIcon}`"
+                    @click:append-inner="showPassword = !showPassword"
+                    variant="outlined"
+                    hint="Enter your password to access this website"
+                    :type="showPassword ? 'text' : 'password'"
+                    counter
+                    maxlength="32"
+                    density="compact"
+                    class="medium_icon"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col class="d-flex align-center justify-center">
+                  <v-btn color="primary" width="100%">Signup</v-btn>
+                </v-col>
+              </v-row>
 
-                <v-row>
-                  <v-col>
-                    <div class="text-black text-center">
-                      Already have an account? <a href="#">Login</a>
-                    </div>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-            </v-form>
-          </v-card>
-        </v-sheet>
+              <v-row>
+                <v-col>
+                  <div class="text-black text-center">
+                    Already have an account? <a href="#">Login</a>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-form>
+        </v-card>
       </v-sheet>
     </div>
   </div>
@@ -121,13 +115,16 @@ export default {
   padding-bottom: 6px !important;
 }
 .small_icon .v-icon {
-    font-size: 12px;
-    top: 34%;
-    transform: translateY(-50%);
+  font-size: 12px;
+  top: 34%;
+  transform: translateY(-50%);
 }
 .medium_icon .v-icon {
-    font-size: 16px;
-    top: 34%;
-    transform: translateY(-50%);
+  font-size: 16px;
+  top: 34%;
+  transform: translateY(-50%);
+}
+.v-icon--clickable {
+  padding: 15px;
 }
 </style>
