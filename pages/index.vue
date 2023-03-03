1d35830383c3613a3ba8f83901d3f2014f3084b4
Leonardo Salas <leosalass@gmail.com>
2023-03-02 21:59:27 -0500

    base nuxt with vuetify

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <template>
+      <div class="d-flex justify-space-around align-center flex-column flex-sm-row fill-height">
+      </div>
+    </template>
+  </div>
+</template>
+<script lang="ts">
+export default {};
+</script>
