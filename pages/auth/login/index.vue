4a115a629f947fc1b1482596039fb08399aa6ba1
Leonardo Salas <leosalass@gmail.com>
2023-03-12 23:27:48 -0500

    showing testing messages

---
+++ pages/auth/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtLayout name="auth">
+      <v-row>
+        <v-col class="text-center"></v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-center"><h1>Login</h1></v-col>
+      </v-row>
+    </NuxtLayout>
+  </template>
+
+  <script setup lang="ts">
+  definePageMeta({
+    layout: false,
+  });
+  </script>
